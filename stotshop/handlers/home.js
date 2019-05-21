eaf716ab47b150d662a1c70254ab0dc8f33c610c
Dimitar Dimitrov <dydimitrov86@gmail.com>
2019-05-21 09:52:30 +0300

    Installed express js on stopshop project and refactored code for controllers

--- stotshop/handlers/home.js
+++ stotshop/handlers/home.js
@@ -1,51 +1,46 @@
-const url = require('url');
-const qs = require('querystring');
 const fs = require('fs');
 const path = require('path');
 const Product = require('../models/Product');
 
-function homeHandler(req, res) {
-    req.pathname = req.pathname || url.parse(req.url).pathname;
+module.exports.index = (req, res) => {
+    let filePath = path.normalize(
+        path.join(__dirname, '../views/home/index.html'));
 
-    if (req.pathname === '/' || req.method === 'GET') {
-        let filePath = path.normalize(path.join(__dirname, '../views/home/index.html'));
-
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                console.log(err);
-                res.writeHead(404, {
-                    'content-type': 'text/plain'
-                });
-                res.write(data);
-                res.end();
-                return;
-            }
-            res.writeHead(200, {
-                'content-type': 'text/html'
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.writeHead(404, {
+                'content-type': 'text/plain'
             });
-            let queryData = qs.parse(url.parse(req.url).query);
-            Product.find().then((products)=>{
-                if (queryData.query){
-                    products = products.filter(product => product.name.toLowerCase().includes(queryData.query))
-                }
+            res.write('404 not found');
+            res.end();
+            return;
+        }
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        });
 
-                let content = '';
-                for (const product of products) {
-                    content += `<div class="product-card">
+        let queryData = req.query;
+
+        Product.find().then((products) => {
+            if (queryData.query) {
+                products = products.filter(
+                    product => product.name.toLowerCase()
+                        .includes(queryData.query))
+            }
+
+            let content = '';
+            for (const product of products) {
+                content += `<div class="product-card">
                      <img class="product-card" src="${product.image}">
                      <h2>${product.name}</h2>
                      <p>${product.description}</p>
                     </div>`
-                }
+            }
 
-                let html = data.toString().replace('{content}', content);
-                res.write(html);
-                res.end();
-            });
-        })
-    } else {
-        return true;
-    }
-}
-
-module.exports = homeHandler;
+            let html = data.toString().replace('{content}', content);
+            res.write(html);
+            res.end();
+        });
+    })
+};
--- stotshop/handlers/index.js
+++ stotshop/handlers/index.js
@@ -1,11 +1,9 @@
 const homeHandler = require('./home');
-const staticHandler = require('./static-files');
 const productHandler = require('./product');
 const categoryHandler = require('./category');
 
-module.exports = [
-    staticHandler,
-    productHandler,
-    categoryHandler,
-    homeHandler
-];
+module.exports = {
+    home: homeHandler,
+    product: productHandler,
+    category: categoryHandler
+};
