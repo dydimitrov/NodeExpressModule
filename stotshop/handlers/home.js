51e241f1bef52454b55a6db775f28679fee39260
Dimitar Dimitrov <dydimitrov86@gmail.com>
2019-05-29 11:45:46 +0300

    Added pug view engine

--- stotshop/handlers/home.js
+++ stotshop/handlers/home.js
@@ -3,44 +3,14 @@ const path = require('path');
 const Product = require('../models/Product');
 
 module.exports.index = (req, res) => {
-    let filePath = path.normalize(
-        path.join(__dirname, '../views/home/index.html'));
-
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.writeHead(404, {
-                'content-type': 'text/plain'
-            });
-            res.write('404 not found');
-            res.end();
-            return;
-        }
-        res.writeHead(200, {
-            'content-type': 'text/html'
-        });
-
         let queryData = req.query;
 
-        Product.find().then((products) => {
+        Product.find().populate('category').then((products) => {
             if (queryData.query) {
                 products = products.filter(
                     product => product.name.toLowerCase()
                         .includes(queryData.query))
             }
-
-            let content = '';
-            for (const product of products) {
-                content += `<div class="product-card">
-                     <img class="product-card" src="${product.image}">
-                     <h2>${product.name}</h2>
-                     <p>${product.description}</p>
-                    </div>`
-            }
-
-            let html = data.toString().replace('{content}', content);
-            res.write(html);
-            res.end();
-        });
-    })
+            res.render('home/index',{products:products})
+    });
 };
--- stotshop/handlers/product.js
+++ stotshop/handlers/product.js
@@ -4,32 +4,12 @@ const Product = require('../models/Product');
 const Category = require('../models/Category');
 
 module.exports.addGet = (req, res) => {
-    let filePath = path.normalize(path.join(__dirname, `../views/products/add.html`));
-
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-
-        Category.find().then((categories) => {
-            let replacment = '<select class="input-field" name="category">';
-            for (let category of categories) {
-                replacment += `<option value="${category.id}">${category.name}</option>`;
-            }
-            replacment += '</select>';
-
-            let html = data.toString().replace('{categories}', replacment);
-            res.writeHead(200, {
-                'content-type': 'text-html'
-            });
-            res.write(html);
-            res.end();
-        });
+    Category.find().then((categories) => {
+        res.render('products/add',{categories: categories})
     });
 };
 
-module.exports.addPost = (req,res) =>{
+module.exports.addPost = (req, res) => {
     let productObj = req.body;
     productObj.image = '\\' + req.file.path;
 
