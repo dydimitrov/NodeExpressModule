f67ba84169e488393a2563c37f5fc0731ee8c6ec
Dimitar Dimitrov <dydimitrov86@gmail.com>
2019-06-06 11:24:06 +0300

    added details view

--- stotshop/config/routes.js
+++ stotshop/config/routes.js
@@ -9,6 +9,10 @@ module.exports = (app) =>{
     app.get('/product/add', handlers.product.addGet);
     app.post('/product/add', upload.single('image'),handlers.product.addPost);
 
+    app.get('/category/:category/products', handlers.category.productByCategory);
+
     app.get('/category/add', handlers.category.addGet);
     app.post('/category/add', handlers.category.addPost);
+
+
 };
