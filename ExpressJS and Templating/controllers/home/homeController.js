43469896be920d72f5483ecee1b897c886f421d0
Dimitar Dimitrov <dydimitrov86@gmail.com>
2019-09-27 09:22:17 +0300

    small fixes

--- ExpressJS and Templating/controllers/home/homeController.js
+++ ExpressJS and Templating/controllers/home/homeController.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
+const qs = require('querystring');
 
 function getHome (req, res) {
     var db = JSON.parse(fs.readFileSync('./config/database.json'));
-
-    res.render('home/index',{db});
+        res.render('home/index',{db});
 };
 
 function getAbout (req, res){
